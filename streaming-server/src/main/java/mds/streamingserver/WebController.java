51cc9632a69d8beca082fb5241267ee3edd84ec7
Viktor Buzovsky <99754196+PrimalMight@users.noreply.github.com>
2022-10-31 12:29:09 +0100

    dadad
    adadada

--- streaming-server/src/main/java/mds/streamingserver/WebController.java
+++ streaming-server/src/main/java/mds/streamingserver/WebController.java
@@ -6,6 +6,7 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.HandlerMapping;
 import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.ServletException;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystem;
+import java.util.Objects;
 
 @Controller
 public class WebController {
@@ -60,4 +62,51 @@ public class WebController {
         model.addAttribute("width",width);
         return "player";
     }
+
+
+    //------------------------------------ ADAPTIVNI STREAM ---------------------------------------------------
+
+    private final static String HLS_PATH = "D:\\MDS\\files\\streams\\HLS\\";
+    private final static String DASH_PATH = "D:\\MDS\\files\\streams\\MPEG-DASH\\";
+
+
+    @RequestMapping(value = {"/dash/{file}", "/hls/{file}", "/hls/{quality}/{file}"}, method = RequestMethod.GET)
+    public void adaptive_streaming(@PathVariable("file") String file, @PathVariable(value = "quality", required = false) String quality, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        File STREAM_FILE = null;
+
+        String handle = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+
+        switch (handle){
+            case "/dash/{file}":
+                STREAM_FILE = new File(DASH_PATH + file);
+                break;
+            case "/hls/{file}":
+                STREAM_FILE = new File(HLS_PATH + file);
+                break;
+            case "/hls/{quality}/{file}":
+                STREAM_FILE = new File(HLS_PATH + quality + "\\"+ file);
+                break;
+            default:
+                STREAM_FILE = null;
+        }
+
+        request.setAttribute(MyResourceHttpRequestHandler.ATTR_FILE, STREAM_FILE);
+        handler.handleRequest(request, response);
+    }
+
+
+    @RequestMapping(value = "dashPlayer", method = {RequestMethod.GET, RequestMethod.POST})
+    public String dashPlayer(Model model, @RequestParam String choose){
+        model.addAttribute("url", choose);
+
+        return "dashPlayer";
+    }
+
+    @RequestMapping(value = "gallery", method = {RequestMethod.GET})
+    public String gallery(){
+
+        return "gallery";
+    }
+
 }
