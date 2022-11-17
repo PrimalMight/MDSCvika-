6c60bad0aa75616152afcca01ac4e7be6d0cf19f
Viktor Buzovsky <99754196+PrimalMight@users.noreply.github.com>
2022-11-17 10:01:09 +0100

    Delete project directory

--- project/src/main/java/mds/project/FilePaths.java
+++
@@ -1,8 +0,0 @@
-package mds.project;
-
-public class FilePaths {
-
-    public final static String VIDEO_FROM_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-    public final static String DASH_DIRECTORY = "D:\\MDS\\files\\project\\streams\\";
-
-}
